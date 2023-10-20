26234c9d3a20fb52086b39937a1675f8b54b0acb
Elijah Edun <edunelijah18@gmail.com>
2023-10-20 13:26:10 +0100

    docs: added airbnb and apache js files

---
+++ awesome-oss-docs/src/constants/docs/orderByAlphabet/apache.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'Apache Open Source Documentation',
+  documentations: [
+    {
+      title: 'Airflow Docs',
+      description:
+        'A platform to programmatically author, schedule, and monitor workflows.',
+      projectUrl: 'https://airflow.incubator.apache.org/docs/',
+      githubUrl: 'https://github.com/apache/airflow/',
+      isOpensource: true,
+      owner: 'Community Owned Documentation',
+    },
+  ],
+};
