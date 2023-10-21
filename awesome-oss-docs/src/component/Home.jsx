f5d198b382931b085449eee32f890918e9b83e0e
wonderful-coyote <wiz0isaac@gmail.com>
2023-10-21 02:31:33 +0100

    search and filter bar feature

--- awesome-oss-docs/src/component/Home.jsx
+++ awesome-oss-docs/src/component/Home.jsx
@@ -1,16 +1,20 @@
+import React from 'react'
 import Header from './Header'
-import Card from './card'
-
+import Card from './Card'
+import SearchFilter from './searchFilter'
 
 const Home = () => {
   return (
     <>
-    <div className=' bg-bgColor h-screen w-screen py-7 px-40'>
-        <Header/>
-    </div>
-    <div>
-      <Card />
-    </div>
+      <div className='bg-bgColor h-screen w-screen py-7 px-40'>
+        <Header />
+      </div>
+      <div className='w-screen py-7 px-40'>
+        <SearchFilter />
+      </div>
+      <div>
+        <Card />
+      </div>
     </>
   )
 }
