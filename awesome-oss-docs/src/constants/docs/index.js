baa24cca27ae8d039461fe2751f2296fb63b3777
Ogunmisi Oluwafemi <ogunmisioluwafemi@gmail.com>
2023-10-24 10:46:54 +0100

    feat:added mayadata dcumentation to website

--- awesome-oss-docs/src/constants/docs/index.js
+++ awesome-oss-docs/src/constants/docs/index.js
@@ -2,6 +2,6 @@ import airbnb from "./orderByAlphabet/airbnb";
 import apache from "./orderByAlphabet/apache";
 import apple from "./orderByAlphabet/apple";
 import docker from "./orderByAlphabet/docker";
+import mayadata from "./orderByAlphabet/mayadata";
 
-
-export default [ airbnb, apache, apple, docker ];
+export default [airbnb, apache, apple, docker, mayadata];
