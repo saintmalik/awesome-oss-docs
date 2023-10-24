e2324e3c363bccaf4481c8079a5bf615f7784f6a
Taran Mittal <taranmittal.1717@gmail.com>
2023-10-24 13:58:40 +0530

    added elastic docs

--- awesome-oss-docs/src/constants/docs/index.js
+++ awesome-oss-docs/src/constants/docs/index.js
@@ -2,6 +2,8 @@ import airbnb from "./orderByAlphabet/airbnb";
 import apache from "./orderByAlphabet/apache";
 import apple from "./orderByAlphabet/apple";
 import docker from "./orderByAlphabet/docker";
+import elastic from "./orderByAlphabet/elastic";
 
 
-export default [ airbnb, apache, apple, docker ];
+
+export default [ airbnb, apache, apple, docker ,elastic];
