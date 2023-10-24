1f24232ec2abe2742ec973d642735983d2ff167a
nobledeveloper01 <nobleconcepts22@gmail.com>
2023-10-24 20:09:21 +0100

    Created facebook.js file and added all Facebook docs in the README.md to the file

--- awesome-oss-docs/src/constants/docs/index.js
+++ awesome-oss-docs/src/constants/docs/index.js
@@ -3,7 +3,8 @@ import apache from "./orderByAlphabet/apache";
 import apple from "./orderByAlphabet/apple";
 import docker from "./orderByAlphabet/docker";
 import elastic from "./orderByAlphabet/elastic";
+import facebook from "./orderByAlphabet/facebook";
 
 
 
-export default [ airbnb, apache, apple, docker ,elastic];
+export default [ airbnb, apache, apple, docker ,elastic, facebook];
