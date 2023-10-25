4e008977533645e8ba270cec9710c03c29809008
Ogunmisi Oluwafemi <ogunmisioluwafemi@gmail.com>
2023-10-25 11:47:28 +0100

    feat:created and added all bridgecrew.js file to the index.js

--- awesome-oss-docs/src/constants/docs/index.js
+++ awesome-oss-docs/src/constants/docs/index.js
@@ -1,6 +1,7 @@
 import airbnb from "./orderByAlphabet/airbnb";
 import apache from "./orderByAlphabet/apache";
 import apple from "./orderByAlphabet/apple";
+import bridgecrew from "./orderByAlphabet/bridgecrew";
 import docker from "./orderByAlphabet/docker";
 import dropbox from "./orderByAlphabet/dropbox";
 import elastic from "./orderByAlphabet/elastic";
@@ -10,6 +11,17 @@ import linode from "./orderByAlphabet/linode";
 import loginradius from "./orderByAlphabet/loginradius";
 import mayadata from "./orderByAlphabet/mayadata";
 
-
-
-export default [ airbnb, apache, apple, docker , dropbox, elastic, facebook, jetbrains, linode, loginradius, mayadata];
+export default [
+  airbnb,
+  apache,
+  apple,
+  bridgecrew,
+  docker,
+  dropbox,
+  elastic,
+  facebook,
+  jetbrains,
+  linode,
+  loginradius,
+  mayadata,
+];
---
+++ awesome-oss-docs/src/constants/docs/orderByAlphabet/bridgecrew.js
@@ -0,0 +1,29 @@
+export default {
+  title: "Bridgecrew Open Source Documentation",
+  documentations: [
+    {
+      title: "AirIAM",
+      description:
+        "AirIAM is a tool to identify and automate Least privilege IAM principles in AWS using Terraform.",
+      projectUrl: "https://airiam.io/documentation",
+      githubUrl: "https://github.com/bridgecrewio/AirIAM/edit/master/docs",
+      owner: "Community Owned Documentation",
+    },
+    {
+      title: "Checkov",
+      description:
+        "Checkov is a static code analysis tool for infrastructure-as-code.",
+      projectUrl: "https://www.checkov.io",
+      githubUrl: "https://github.com/bridgecrewio/checkov/tree/master/docs",
+      owner: "Community Owned Documentation",
+    },
+    {
+      title: "Yor",
+      description:
+        "Yor is an open-source tool that helps add informative and consistent tags across infrastructure-as-code frameworks such as Terraform, CloudFormation, and Serverless.",
+      projectUrl: "https://yor.io/",
+      githubUrl: "https://github.com/bridgecrewio/yor/tree/main/docs",
+      owner: "Community Owned Documentation",
+    },
+  ],
+};
