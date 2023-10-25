35bd5b82b00fcd5bf424b3f810dae562f1b83892
wonderful-coyote <wiz0isaac@gmail.com>
2023-10-25 01:35:17 +0100

    added linode docs

--- awesome-oss-docs/src/constants/docs/index.js
+++ awesome-oss-docs/src/constants/docs/index.js
@@ -3,7 +3,8 @@ import apache from "./orderByAlphabet/apache";
 import apple from "./orderByAlphabet/apple";
 import docker from "./orderByAlphabet/docker";
 import elastic from "./orderByAlphabet/elastic";
+import linode from "./orderByAlphabet/linode";
 
 
 
-export default [ airbnb, apache, apple, docker ,elastic];
+export default [ airbnb, apache, apple, docker ,elastic, linode];
---
+++ awesome-oss-docs/src/constants/docs/orderByAlphabet/linode.js
@@ -0,0 +1,15 @@
+export default {
+    title: 'Linode Open Source Documentation',
+    documentations: [
+      {
+        title: 'Linode Docs',
+        description:
+          'Linode guides and tutorials.',
+        projectUrl: 'https://www.linode.com/docs/',
+        githubUrl: 'https://github.com/linode/docs',
+
+        owner: 'Company Owned Documentation',
+      },
+    ],
+  };
+  
