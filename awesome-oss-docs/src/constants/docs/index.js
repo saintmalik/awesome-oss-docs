2fdadc1e9d7b61861ec2c84bb01954ef2814e44a
Medha A B <medha1ga19cs087@gmail.com>
2023-10-26 10:08:18 +0530

    Added HashiCorps Documentation Entries

--- awesome-oss-docs/src/constants/docs/index.js
+++ awesome-oss-docs/src/constants/docs/index.js
@@ -2,6 +2,6 @@ import airbnb from "./orderByAlphabet/airbnb";
 import apache from "./orderByAlphabet/apache";
 import apple from "./orderByAlphabet/apple";
 import docker from "./orderByAlphabet/docker";
+import hashicorpDocs from "./orderByAlphabet/hashicorpDocs";
 
-
-export default [ airbnb, apache, apple, docker ];
+export default [ airbnb, apache, apple, docker, hashicorpDocs];
