35131f1de7a1ada5b62058d8bf64a93f72e02337
vikrantsingh22 <vikrantsingh2020004@gmail.com>
2023-10-29 12:15:01 +0530

    Feat:Added-suse.js-inside-docs

---
+++ awesome-oss-docs/src/constants/docs/orderByAlphabet/suse.js
@@ -0,0 +1,39 @@
+export default {
+  title: "SUSE Open Source Documentation",
+  documentations: [
+    {
+      title: "Longhorn Docs",
+      description: "A cloud native distributed block storage for Kubernetes.",
+      projectUrl: "https://longhorn.io/docs/",
+      githubUrl:
+        "https://github.com/longhorn/website/edit/master/content/docs/1.5.1/_index.md",
+      owner: "Community Owned Documentation",
+    },
+    {
+      title: "Trento Docs",
+      description:
+        "A cloud native web console for SAP Applications system administrators tasks.",
+      projectUrl: "https://shopify.github.io/FunctionalTableData/",
+      githubUrl: "https://github.com/trento-project/trento/tree/main/docs",
+      owner: "Community Owned Documentation",
+    },
+    {
+      title: "Uyuni Docs",
+      description:
+        " Uyuni is a solution for organizations that require robust control over maintenance and package deployment on their servers.",
+      projectUrl:
+        "https://www.uyuni-project.org/uyuni-docs/en/uyuni/index.html",
+      githubUrl: "",
+      owner: "Community Owned Documentation",
+    },
+    {
+      title: "Kubewarden Docs ",
+      description:
+        " Kubewarden is a policy engine for Kubernetes. Its mission is to simplify the adoption of policy-as-code.",
+      projectUrl: "https://docs.kubewarden.io/",
+      githubUrl:
+        "https://github.com/kubewarden/docs/edit/main/versioned_docs/version-1.8/introduction.md",
+      owner: "Community Owned Documentation",
+    },
+  ],
+};
