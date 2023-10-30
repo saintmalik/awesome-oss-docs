f71463c0215f85832aa95ab20bebf8fb39b126ca
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-10-30 18:42:59 +0600

    [Enhancement] Added the Guild Docs as mentioned in the README.md into theguild.js file

---
+++ awesome-oss-docs/src/constants/docs/orderByAlphabet/theguild.js
@@ -0,0 +1,48 @@
+export default {
+    title: "Guild Open Source Documentation",
+    documentations: [
+        {
+            title: "GraphQL Inspector Docs",
+            description:
+                "Validate schema, get schema change notifications, validate operations, find breaking changes, look for similar types, schema coverage.",
+            projectUrl: "https://graphql-inspector.com/docs/index",
+            owner: "Community Owned Documentation",
+        },
+        {
+            title: "GraphQL Config Docs",
+            description:
+                "One configuration for all your GraphQL tools supported by most tools, editors & IDEs.",
+            projectUrl: "https://graphql-config.com/introduction",
+            owner: "Community Owned Documentation",
+        },
+        {
+            title: "GraphQL Modules Docs",
+            description: "Enterprise Grade Tooling For Your GraphQL Server.",
+            projectUrl: "https://www.graphql-modules.com/docs/index",
+            owner: "Community Owned Documentation",
+        },
+        {
+            title: "GraphQL Mesh Docs",
+            description: "Query anything, run anywhere.",
+            projectUrl:
+                "https://www.graphql-mesh.com/docs/getting-started/introduction",
+            owner: "Community Owned Documentation",
+        },
+        {
+            title: "GraphQL Code Generator Docs",
+            description:
+                'A tool for generating code based on a GraphQL schema and GraphQL operations "query/mutation/subscription", with flexible support for custom plugins.',
+            projectUrl:
+                "https://www.graphql-code-generator.com/docs/getting-started/index",
+            owner: "Community Owned Documentation",
+        },
+        {
+            title: "GraphQL Tools Docs",
+            description:
+                "Build, mock, and stitch a GraphQL schema using the schema language.",
+            projectUrl: "https://www.graphql-tools.com/docs/introduction",
+            owner: "Community Owned Documentation",
+        },
+    ],
+  };
+  
