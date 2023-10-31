fd85bd8d5e3fc72ef828799657c5d3dac7f7e057
IfeOluwa Akanwo <146724964+menorahh@users.noreply.github.com>
2023-10-31 23:58:50 +0100

    Create salesforce.js

---
+++ awesome-oss-docs/src/constants/docs/orderByAlphabet/salesforce.js
@@ -0,0 +1,61 @@
+export default {
+  title: 'Salesforce Open Source Documentation',
+  documentations: [
+    {
+      title: 'Lightning Web Components Docs',
+      description:
+        'A Blazing Fast, Enterprise-Grade Web Components Foundation, The lightweight, high performance web component framework that powers thousands of apps.',
+      projectUrl: 'https://lwc.dev/guide/introduction',
+      githubUrl: '',
+      owner: 'Community Owned Documentation',
+    },
+    {
+      title: 'Buildpacks Docs',
+      description:
+        "Buildpacks are pluggable, modular tools that translate source code into OCI images.",
+      projectUrl: 'https://buildpacks.io/docs/',
+      githubUrl: '',
+      owner: 'Community Owned Documentation',
+    },
+    {
+      title: 'Cloudsplaining Docs',
+      description:
+        'AWS IAM security assessment tool that identifies violations of least privilege and generates a risk-prioritized report.',
+      projectUrl: 'https://cloudsplaining.readthedocs.io/en/latest/',
+      githubUrl: '',
+      owner: 'Company Owned Documentation',
+    },
+    {
+      title: 'Oclif Docs',
+      description:
+      'A framework for building CLIs in Node.js. Designed both for simple CLIs that can be just a single file with a few flag options, or for very complex CLIs that have many subcommands.',
+      projectUrl: 'https://oclif.io/docs/introduction',
+      githubUrl: '',
+      owner: 'Company Owned Documentation',
+    },
+    {
+      title: 'Apache PredictionIO Doc',
+      description:
+        'Apache PredictionIO® is a machine learning server for developers and data scientists to create predictive engines for any machine learning task. ',
+      projectUrl: 'https://predictionio.apache.org/start/',
+      githubUrl: '',
+      owner: 'Community Owned Documentation',
+    },
+    {
+      title: 'TransmogrifAI Docs',
+      description:
+        'AutoML library for building modular, reusable, strongly typed machine learning workflows on Apache Spark with minimal hand tuning.',
+      projectUrl: 'https://docs.transmogrif.ai/en/stable/',
+      githubUrl: 'https://github.com/salesforce/TransmogrifAI/tree/master/docs',
+      owner: 'Company Owned Documentation',
+    },
+    {
+      title: 'Policy Sentry Docs',
+      description:
+        'AM Least Privilege Policy Generator.',
+      projectUrl: 'https://policy-sentry.readthedocs.io/en/latest/',
+      githubUrl: '',
+      owner: 'Community Owned Documentation',
+    },
+  ],
+};
