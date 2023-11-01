546a1c561055dcd4f851d97b6c9ea9fdefbd451d
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-11-01 10:49:25 +0600

    [Fix] Imported redhat from redhat.js into index.js

--- awesome-oss-docs/src/constants/docs/index.js
+++ awesome-oss-docs/src/constants/docs/index.js
@@ -21,6 +21,7 @@ import mayadata from "./orderByAlphabet/mayadata";
 import netflix from "./orderByAlphabet/netflix";
 import netlify from "./orderByAlphabet/netlify";
 import ory from "./orderByAlphabet/ory";
+import redhat from './orderByAlphabet/redhat';
 import salesforce from "./orderByAlphabet/salesforce";
 import samsung from "./orderByAlphabet/samsung";
 import shopify from "./orderByAlphabet/shopify";
@@ -60,6 +61,7 @@ export default [
   netflix,
   netlify,
   ory,
+  redhat,
   salesforce,
   samsung,
   shopify,
