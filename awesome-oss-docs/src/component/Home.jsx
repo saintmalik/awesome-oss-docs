b110be33708458630724792311e3185f11842217
wonderful-coyote <wiz0isaac@gmail.com>
2023-11-05 21:50:39 +0100

    making the awesome docs responsive

--- awesome-oss-docs/src/component/Home.jsx
+++ awesome-oss-docs/src/component/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import Card from './Card';
+import Card from './card';
 import SearchFilter from './SearchFilter';
 
 const Home = () => {
