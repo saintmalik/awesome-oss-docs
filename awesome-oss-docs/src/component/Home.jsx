a94c8b745996241d031e8bbb5be1babe171e4ad6
wonderful-coyote <wiz0isaac@gmail.com>
2023-11-06 15:12:10 +0100

    adjusted the header

--- awesome-oss-docs/src/component/Home.jsx
+++ awesome-oss-docs/src/component/Home.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import Header from './Header';
-import Card from './card';
-import SearchFilter from './searchFilter';
+import React from 'react'
+import Header from './Header'
+import Card from './card'
+import SearchFilter from './searchFilter'
 
 const Home = () => {
   return (
-    <div className="bg-white min-h-screen py-7 px-4 md:px-10 lg:px-20">
-      <Header />
-      <div className="container mx-auto">
-        <div className="py-7 md:py-10 lg:py-16">
-          <SearchFilter />
-        </div>
-        <div>
-          <Card />
-        </div>
+    <>
+      <div className='bg-bgColor h-50 w-screen py-7 px-10'>
+        <Header />
       </div>
-    </div>
-  );
-};
+      <div className='w-screen py-7 px-10'>
+        <SearchFilter />
+      </div>
+      <div>
+        <Card />
+      </div>
+    </>
+  )
+}
 
-export default Home;
+export default Home
